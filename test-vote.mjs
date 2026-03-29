7061fcc4a7e70cefb657b786b39a6d673bd2fcad
rohan <rohanrv2004@gmail.com>
2026-03-29 13:21:47 +0530

    pnpm

---
+++ test-vote.mjs
@@ -0,0 +1,1 @@
+fetch('http://localhost:3000/api/vote', { method: 'POST', headers: {'Content-Type': 'application/json', 'x-forwarded-for': Math.random().toString()}, body: JSON.stringify({pollId: 'cmnbdh7g00005106oi9c5e1f9', optionId: 'cmnbdh7g00005106oi9c5e1f9', fingerprintClient: Math.random().toString()}) }).then(r=>r.text()).then(console.log).catch(console.error)
